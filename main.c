fb06cb0eced56a149b8a9391c01bf54c92009b6d
Francisco Cejas <francejas98@gmail.com>
2025-04-10 04:21:57 -0300

    Añadir mi código inicial

---
+++ main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int selected;
+
+    do
+    {
+        selected = menu();
+        switch(selected)
+        {
+        case 1:
+            funcion_x();
+            break;
+
+        case 0:
+            printf("\n\nTERMINATE THE PROGRAM\n");
+
+            break;
+        }
+
+    }
+    while(selected!=0);
+
+    system("PAUSE");
+    return 0;
+}
+int menu()
+{
+    system("color 1F");
+    int input;
+    system("cls");
+    printf("\nLABORATORIO 1");
+    printf("\n----------");
+    printf("\nLIST MENU");
+    printf("\n-----------");
+    printf("\n1-Agrega elementos a Pila");
+
+    printf("\n\nENTER YOUR CHOICE: ");
+    scanf("%d",&input);
+    return input;
+}
+
